1a9ca48b1f826f4e2275dcca8455580ce3864cec
Niharika Golla <159674661+Niharika-Golla@users.noreply.github.com>
2024-11-03 18:06:28 +0530

    Delete AdminComponent/src/main/java/com/main/utils directory

--- AdminComponent/src/main/java/com/main/repository/PondRepository.java
+++
@@ -1,7 +0,0 @@
-package com.main.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.main.model.Pond;
-
-public interface PondRepository extends MongoRepository<Pond, String> {
-}
